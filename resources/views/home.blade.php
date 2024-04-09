6c01065e457611bc8b2b21f15b318457c6497455
giosciafra <francescogioscia8@gmail.com>
2024-04-09 18:05:00 +0200

    pagina stilizzata ed esercizio completato con bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,24 +2,28 @@
 
 @section('content')
 
+<div class="container py-5">
 
+    <h1 class="text-center pb-5 text-uppercase text-primary ">Ecco i film nel nostro database!</h1>
 
+    <div class="d-flex flex-wrap gap-4 ">
 
-<div class="container py-5 ">
-
-    <div class="card d-flex " style="width: 18rem;">
         @foreach($movies as $movie)
-    
-        {{-- <img src="..." class="card-img-top" alt="..."> --}}
-        <div class="card-body">
-          <h5 class="card-title">{{$movie->title}}</h5>
-          <p class="card-text"> {{$movie->vote}}/10</p>
+        <div class="card " >
+            
+            {{-- <img src="..." class="card-img-top" alt="..."> --}}
+            <div class="card-body">
+                <h2 class="card-title"><strong>{{$movie->title}}</strong></h5>
+                <p class="card-text"><strong>Titolo Originale:</strong> {{$movie->original_title}}</p>
+                <p class="card-text first-letter"><strong>Nazionalità:</strong> {{$movie->nationality}}</p>
+                <p class="card-text"><strong>Data di Uscita:</strong> {{$movie->date}}</p>
+
+                <p class="card-text"><strong>Voto iMDb:</strong> {{$movie->vote}}/10</p>
+
+            </div>
+            
         </div>
         @endforeach
-    
-      </div>
-    </div> 
-
-    
-</div>
+    </div>
+</div> 
 @endsection
