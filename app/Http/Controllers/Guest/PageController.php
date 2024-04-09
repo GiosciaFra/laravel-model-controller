ea7a00ff1e916dcd609b774d6a7f67f346465518
giosciafra <francescogioscia8@gmail.com>
2024-04-09 13:13:28 +0200

    creato model e recuperato tutti i film (stampa momentanea in dd)

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
+
+        $movies = Movie::all();
+
+        dd($movies);
         return view("home");
     }
 }
